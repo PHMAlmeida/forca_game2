40ceca186652f61726ed44355150c7ff47b4629b
phmalmeida <pepemachado10@hotmail.com>
2022-02-21 10:04:36 -0300

    :truck: Rename file

--- pages/api/ranking.js
+++ pages/api/ranking.js
@@ -16,6 +16,5 @@ export default async (req, res) => {
     const db = await connectDb(process.env.MONGODB_URI);
 
     const data = await db.collection('users').find({}).sort({score:-1}).toArray();
-    console.log(data);
     res.status(200).json(data);
 }
